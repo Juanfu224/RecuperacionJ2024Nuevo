26b15e04d39a252f1040be793ef80f5ffc6d73ef
Juanfu224 <juanfu224@gmail.com>
2024-06-18 16:21:25 +0200

    añadir clase a Ventana

---
+++ Ventana.java
@@ -0,0 +1,14 @@
+
+/**
+ * Ventana
+ */
+public class Ventana {
+
+    public static void saludar(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        saludar();
+    }
+}
